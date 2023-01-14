b32c1b943ad33d032de9f6436fcfa80c8e050689
sanyar123 <54771451+sanyar123@users.noreply.github.com>
2023-01-14 14:25:23 +0100

    Update script.js

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -1,6 +1,8 @@
 module.exports = async({ context, github, core }) => {
             console.log('commit hash value:');
             console.log(`${github.event.deployment.sha}`);
+            console.log(`${github}`);
+            console.log(`${context}`);
             const result = await github.rest.actions.createWorkflowDispatch({
               owner: `${ process.env.OWNER }`,
               repo: `${ process.env.REPO }`,
